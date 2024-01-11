beafcc71c361f4eaac5e26884ccedbc496517236
ds-Kirill <73669383+ds-Kirill@users.noreply.github.com>
2024-01-12 00:36:09 +0300

    Create train_dag.py
    dag for train model with airflow and lflow

---
+++ hw5/train_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+}
+
+dag = DAG('train_model', default_args=default_args, schedule_interval='0 0 * * 0')
+
+ssh_task = SSHOperator(
+    task_id='ssh_command',
+    ssh_conn_id='sshconn',
+    command=f'python3 /home/ubuntu/train.py',
+    conn_timeout=72000,
+    cmd_timeout=72000,
+    dag=dag
+)
+
+ssh_task
